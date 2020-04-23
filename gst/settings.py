d4c70661a87071da5958fa97365e6d14de4e3ba6
jekobhai <somnathjena.2011@gmail.com>
2020-04-23 09:49:30 +0530

    Third commit

--- gst/settings.py
+++ gst/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'paypal.standard.ipn',
+    #'paypal.standard.ipn',
     'accounts',
     'ebill',
     'invoice',
@@ -49,8 +49,8 @@ INSTALLED_APPS = [
     'itc',
 ]
 
-PAYPAL_RECEIVER_EMAIL = 'somnathjena.2011@gmail.com'
-PAYPAL_TEST = True
+#PAYPAL_RECEIVER_EMAIL = 'somnathjena.2011@gmail.com'
+#PAYPAL_TEST = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
